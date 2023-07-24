277b044d8332c97e8aa010e32381e0d76932fa88
Evgenia <kbe4989@yandex.ru>
2023-07-24 21:53:52 +0300

    Добавили решение в Task_008/

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите целое число N: ");
+
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите целое число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        if (a % 2! == 0)
+            a+= 1;
+
+        while (a <= N)
+                   
+            {
+            Console.WriteLine(a +1);
+            a+= 2;
+            }
+            
+            
+        
+    }
+}
