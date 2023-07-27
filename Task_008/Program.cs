ac1bd27f95b50ce198b6ac69b527f35a61bb74ba
Evgenia <kbe4989@yandex.ru>
2023-07-27 20:23:10 +0300

    Сохранили изменения в Task_008

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -12,7 +12,7 @@ internal class Program
 
         Console.Write("Введите целое число a: ");
         int a = Convert.ToInt32(Console.ReadLine());
-
+        
         if (a % 2! == 0)
             a+= 1;
 
