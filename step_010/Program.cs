c793d346c2749c54860e228f6e9257fc2492501d
Evgenia <kbe4989@yandex.ru>
2023-07-29 10:37:26 +0300

    Сохранили изменения в step_010

---
+++ step_010/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число");
+        int.TryParse(Console.ReadLine()!, out int x);
+
+        if (x > 999 || x < 100)
+        {
+            Console.WriteLine("Количество знаков не соответсвует трем!");
+            return;
+        }
+        int GetSecond(int x)
+        {
+            while (x >= 100) x /= 10;
+            int a = x % 10;
+            return a;
+        }
+        Console.WriteLine(GetSecond(x));
+    }
+}
