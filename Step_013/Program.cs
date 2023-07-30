10acc142fcb107180968fd76d8179d64b6c9229c
Evgenia <kbe4989@yandex.ru>
2023-07-30 10:58:16 +0300

    Сохранили изменения в Step_013

--- Step_013/Program.cs
+++ Step_013/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число");
+        int.TryParse(Console.ReadLine()!, out int x);
+
+        if (x > 999 || x < 100)
+        {
+            Console.WriteLine("Количество знаков не соответсвует трем!");
+            return;
+        }
+        int GetSecond(int x)
+        {
+            while (x >= 100) x /= 10;
+            int a = x % 10;
+            return a;
+        }
+        Console.WriteLine(GetSecond(x));
+
