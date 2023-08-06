f8fcab16c5ad30315bf66a7f0ced785ddefc057b
Evgenia <kbe4989@yandex.ru>
2023-08-06 10:15:00 +0300

    Дополнили данные в Step_029

---
+++ Step_029/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Задача 29 
+// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 5 -> [1, 2, 5, 7, 19]; 3 -> [6, 1, 33] */
+
+int[] randomArray = new int[8];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
