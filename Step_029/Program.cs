ccf422a95f0cef438fe9f4b068640443aac8d5b2
Evgenia <kbe4989@yandex.ru>
2023-08-06 13:14:15 +0300

    Дополнили данные в Step_29

--- Step_029/Program.cs
+++ Step_029/Program.cs
@@ -1,17 +1,40 @@
-﻿
-// Задача 29 
-// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+﻿// Задача 29 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 5 -> [1, 2, 5, 7, 19]; 3 -> [6, 1, 33] */
 
-int[] randomArray = new int[8];
-for (int i = 0; i < randomArray.Length; i++)
+int Prompt(string message)
 {
-    randomArray[i] = new Random().Next(1,9);
-    Console.Write(randomArray[i] + " ");
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result; 
 }
 
-int ReadInt(string message)
+int MyArray(int length, int minDigit, int maxDigit)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    int [] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minDigit, maxDigit +1);
+    }
+    return array;
 }
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++);
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}, ");
+    System.Console.Write("]");
+}
+
+int length = Prompt("Длина массива: ");
+int minDigit = Prompt("Начальное значение для диапазона случайных числел: ");
+int maxDigit = Prompt("Конечное значение для диапазона случайных числел: ");
+int [] array = MyArray(length, minDigit , maxDigit );
+
+PrintArray(array);
