0d18ada2b1bd92add70420346951e9cad2a085ff
Evgenia <kbe4989@yandex.ru>
2023-09-03 15:11:57 +0300

    Step_068

--- Step-068/Program.cs
+++ Step-068/Program.cs
@@ -1,32 +1,8 @@
-﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-//m = 2, n = 3 -> A(m,n) = 9
-//m = 3, n = 2 -> A(m,n) = 29
+﻿ //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+ //m = 2, n = 3 -> A(m,n) = 9
+ //m = 3, n = 2 -> A(m,n) = 29
 
-// 1 вариант //
-
-int m = InputNumbers("Введите M: ");
-int n = InputNumbers("Введите N: ");
-
-int functionAkkerman = Ack(m, n);
-
-Console.Write($"Функция Аккермана = {functionAkkerman} ");
-
-int Ack(int m, int n)
-{
-    if (m == 0) return n + 1;
-    else if (n == 0) return Ack(m - 1, 1);
-    else return Ack(m - 1, Ack(m, n - 1));
-}
-
-int InputNumbers(string input)
-{
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
-    return output;
-}
-
-// 2 вариант //
-
+ 
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
@@ -36,14 +12,14 @@ int n = Convert.ToInt32(Console.ReadLine());
 AkkermanFunction(m,n);
 
 
-
-void AkkermanFunction(int m, int n)                   // вызов функции Аккермана
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
 {
     Console.Write(Akkerman(m, n)); 
 }
 
-
-int Akkerman( int m, int n)                            // функция Аккермана
+// функция Аккермана
+int Akkerman(int m, int n)
 {
     if (m == 0)
     {
--- Step_058/Program.cs
+++ Step_058/Program.cs
@@ -1,64 +1,86 @@
-﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
-// Результирующая матрица будет:
-// 18 20
-// 15 18
+﻿﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
 
 
-void Print(int [,] arr)
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"Первая матрица: ");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица: ");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение матриц: ");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
 {
-    int rows_size = arr.GetLength(0);
-    int columns_size = arr.GetLength(1);
-    for (int i =0; i < rows_size; i++)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
     {
-        for (int j = 0; j < columns_size; j++)
-            Console.Write($" {arr[i, j],3} ");
-        Console.WriteLine();
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
     }
     Console.WriteLine();
+  }
 }
-int [,] MassNum(int rows, int columns)                  
-{
-    int [,] arr = new int [rows, columns];
-    for (int i = 0; i < rows; i++)
-        for(int j = 0; j < columns; j++)
-            arr[i, j] = new Random().Next(1, 10);
-    return arr;
-}
-
-int [,] MatrixProduct(int[] arr_first, int[] arr_second)
-{
-    int rows_size = arr_first.GetLength(0);
-    int columns_size = arr_second.GetLength(1);
-    
-int [,] prod_matrix = new int [rows_size, columns_size];
-{
-    if (rows_size != arr_second.GetLength(0) || columns_size != arr_second.GetLength(1)) return prod_matrix;
-    {
-    for (int i = 0; i < rows_size; i++);
-        for (int j = 0; j < columns_size; j++)
-            prod_matrix[ i, j] = arr_first[ i, j] * arr_second[ i, j];
-        return prod_matrix;
-    }
-}  
-}
-Console.Write("Введите числа строки 1: ");
-int rows_1 = int.Parse(Console.ReadLine()!);
-Console.Write("Введите числа столбца 1: ");
-int columns_1 = int.Parse(Console.ReadLine()!);
-
-Console.Write("Введите числа строки 2: ");
-int rows_2 = int.Parse(Console.ReadLine()!);
-Console.Write("Введите числа столбца 2: ");
-int columns_2 = int.Parse(Console.ReadLine()!);
-
-int[,] arr_1 = MassNum(rows_1, columns_1);
-Print(arr_1);
-
-int[,] arr_2 = MassNum(rows_2, columns_2);
-Print(arr_2);
-
-int[,] res_matrix = MatrixProduct[arr_1, arr_2];
-Print(res_matrix);
